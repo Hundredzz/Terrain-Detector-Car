bedaea17b120f2d4d0c7e32cfa4ea9dd1de5c894
Ratchanon Yaemboo <toonkaf@gmail.com>
2025-10-31 13:24:57 +0700

    Update test_motor.ino

--- test_motor/test_motor.ino
+++ test_motor/test_motor.ino
@@ -1,7 +1,7 @@
-const int AP 5; //A+
-const int AM 6; //A-
-const int BP 10; //B+
-const int BM 11; //B-
+const int AP=5; //A+
+const int AM=6; //A-
+const int BP=10; //B+
+const int BM=11; //B-
 
 void setup() {
   pinMode(AP,OUTPUT);
@@ -12,32 +12,32 @@ void setup() {
 
 void loop() {
   //forward?
-  digitalWrite(AP,HIGH);
-  digitalWrite(AM,LOW);
-  digitalWrite(BP,LOW);
-  digitalWrite(BM,HIGH);
+  analogWrite(AP,255);
+  analogWrite(AM,0);
+  analogWrite(BP,0);
+  analogWrite(BM,200);
   delay(2000);
 
-  //turn right?
-  digitalWrite(AP,HIGH);
-  digitalWrite(AM,LOW);
-  digitalWrite(BP,HIGH);
-  digitalWrite(BM,LOW);
-  delay(2000);
+  // //turn right?
+  // digitalWrite(AP,HIGH);
+  // digitalWrite(AM,LOW);
+  // digitalWrite(BP,HIGH);
+  // digitalWrite(BM,LOW);
+  // delay(2000);
 
-  //turn left?
-  digitalWrite(AP,LOW);
-  digitalWrite(AM,HIGH);
-  digitalWrite(BP,LOW);
-  digitalWrite(BM,HIGH);
-  delay(2000);
+  // //turn left?
+  // digitalWrite(AP,LOW);
+  // digitalWrite(AM,HIGH);
+  // digitalWrite(BP,LOW);
+  // digitalWrite(BM,HIGH);
+  // delay(2000);
 
-  //backward?
-  digitalWrite(AP,LOW);
-  digitalWrite(AM,HIGH);
-  digitalWrite(BP,HIGH);
-  digitalWrite(BM,LOW);
-  delay(2000);
+  // //backward?
+  // digitalWrite(AP,LOW);
+  // digitalWrite(AM,HIGH);
+  // digitalWrite(BP,HIGH);
+  // digitalWrite(BM,LOW);
+  // delay(2000);
 
   //stop
   digitalWrite(AP,LOW);
