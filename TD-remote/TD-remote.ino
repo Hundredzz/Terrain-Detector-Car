e66fbaa4ad2d795fc6f9e3268fd9a1ceaf55fba6
Ratchanon Yaemboo <toonkaf@gmail.com>
2025-10-31 18:22:28 +0700

    Create TD-remote.ino

---
+++ TD-remote/TD-remote.ino
@@ -0,0 +1,135 @@
+#include <WiFiS3.h>
+#include <MQTTClient.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const char WIFI_SSID[] = "NEROROSE";     // CHANGE TO YOUR WIFI SSID
+const char WIFI_PASSWORD[] = "BlackRose";  // CHANGE TO YOUR WIFI PASSWORD
+
+const char MQTT_BROKER_ADRRESS[] = "mqtt-dashboard.com";  // CHANGE TO MQTT BROKER'S ADDRESS
+//const char MQTT_BROKER_ADRRESS[] = "192.168.0.11";  // CHANGE TO MQTT BROKER'S IP ADDRESS
+const int MQTT_PORT = 1883;
+const char MQTT_CLIENT_ID[] = "TD-car-Sender";  // CHANGE IT AS YOU DESIRE
+const char MQTT_USERNAME[] = "";              // CHANGE IT IF REQUIRED, empty if not required
+const char MQTT_PASSWORD[] = "";              // CHANGE IT IF REQUIRED, empty if not required
+
+// The MQTT topics that Arduino should publish/subscribe
+const char PUBLISH_TOPIC[] = "TD-car/car";       // CHANGE IT AS YOU DESIRE
+const char SUBSCRIBE_TOPIC[] = "TD-car/remote";  // CHANGE IT AS YOU DESIRE
+
+const int PUBLISH_INTERVAL = 100;  // 5 seconds
+
+WiFiClient network;
+MQTTClient mqtt = MQTTClient(256);
+
+unsigned long lastPublishTime = 0;
+
+const int up = 8;
+const int down = 9;
+const int left = 10;
+const int right = 11;
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  Serial.begin(9600);
+  pinMode(up,INPUT);
+  pinMode(down,INPUT);
+  pinMode(left,INPUT);
+  pinMode(right,INPUT);
+
+  int status = WL_IDLE_STATUS;
+  while (status != WL_CONNECTED) {
+    Serial.print("Arduino UNO R4 - Attempting to connect to SSID: ");
+    Serial.println(WIFI_SSID);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    // wait 10 seconds for connection:
+    delay(100);
+  }
+  // print your board's IP address:
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  connectToMQTT();
+}
+
+void loop() {
+  mqtt.loop();
+
+  if (millis() - lastPublishTime > PUBLISH_INTERVAL) {
+    sendToMQTT();
+    lastPublishTime = millis();
+  }
+  // Serial.println(digitalRead(up));
+  
+}
+
+void connectToMQTT() {
+  // Connect to the MQTT broker
+  mqtt.begin(MQTT_BROKER_ADRRESS, MQTT_PORT, network);
+
+  // Create a handler for incoming messages
+  mqtt.onMessage(messageReceived);
+
+  Serial.print("Arduino UNO R4 - Connecting to MQTT broker");
+
+  while (!mqtt.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD)) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+
+  if (!mqtt.connected()) {
+    Serial.println("Arduino UNO R4 - MQTT broker Timeout!");
+    return;
+  }
+
+  // Subscribe to a topic, the incoming messages are processed by messageHandler() function
+  if (mqtt.subscribe(SUBSCRIBE_TOPIC))
+    Serial.print("Arduino UNO R4 - Subscribed to the topic: ");
+  else
+    Serial.print("Arduino UNO R4 - Failed to subscribe to the topic: ");
+
+  Serial.println(SUBSCRIBE_TOPIC);
+  Serial.println("Arduino UNO R4 - MQTT broker Connected!");
+}
+
+void sendToMQTT() {
+  String val_str;
+  if(digitalRead(up) == 0){
+    val_str = "up";
+  }else if(digitalRead(down) == 0){
+    val_str = "down";
+  }else if(digitalRead(left) == 0){
+    val_str = "left";
+  }else if(digitalRead(right) == 0){
+    val_str = "right";
+  }else{
+    return;
+  }
+  char messageBuffer[10];
+  val_str.toCharArray(messageBuffer, 10);
+  mqtt.publish(PUBLISH_TOPIC, messageBuffer);
+  Serial.println("Arduino UNO R4 - sent to MQTT:");
+  Serial.print("- topic: ");
+  Serial.println(PUBLISH_TOPIC);
+  Serial.print("- payload:");
+  Serial.println(messageBuffer);
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("Arduino UNO R4 - received from MQTT:");
+  Serial.println("- topic: " + topic);
+  Serial.println("- payload:");
+  Serial.println(payload);
+  lcd.setCursor(0, 0);
+  if(payload == "smooth"){
+    lcd.print("smooth  ");
+  }else{
+    lcd.print("ROUGH!!!");
+  }
+  lcd.write(byte(6)); 
+}
